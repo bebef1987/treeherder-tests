8f50a2fff091862df38b082d896a3413fa1823ab
Bebe <bebe@mozilla.ro>
2016-03-17 23:07:17 +0200

    Remove test classes
    Remove test classes as they are not necessary on pytests
    
    Fix issue: #34

--- tests/test_view_single_result.py
+++ tests/test_view_single_result.py
@@ -7,12 +7,10 @@ import pytest
 from pages.treeherder import TreeherderPage
 
 
-class TestSingleResult:
-
-    @pytest.mark.nondestructive
-    def test_open_single_result(self, base_url, selenium):
-        page = TreeherderPage(base_url, selenium).open()
-        first_revision_date = page.first_revision_date
-        page.open_single_resultset()
-        assert 1 == page.results_count
-        assert first_revision_date == page.first_revision_date
+@pytest.mark.nondestructive
+def test_open_single_result(base_url, selenium):
+    page = TreeherderPage(base_url, selenium).open()
+    first_revision_date = page.first_revision_date
+    page.open_single_resultset()
+    assert 1 == page.results_count
+    assert first_revision_date == page.first_revision_date
